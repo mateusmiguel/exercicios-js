75ac481629006e76917a35e8e4939ec06b747ee8
Mateus Miguel <webmateusmiguel@gmail.com>
2018-03-12 18:04:20 -0300

    doWhile

--- controle/switch.js
+++ controle/switch.js
@@ -4,8 +4,24 @@ const imprimirResultado = function (nota) {
         case 9:
             console.log('Quadro de Honra')
             break
-        case 7:
-            console.log('ok')
+        case 8: case 7:
+            console.log('Aprovado')
+            break
+        case 6: case 5: case 4:
+            console.log('Recuperação')
+            break
+        case 3: case 2: case 1: case 0:
+            console.log('Reprovado')
+            break
+        default:
+         console.log('nota inválida')
+
     }
 }
-imprimirResultado(3)
+
+imprimirResultado(10)
+// imprimirResultado(8.9)
+// imprimirResultado(6.55)
+// imprimirResultado(-1)
+// imprimirResultado(11)
+
