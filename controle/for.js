694844660d3d0353f2519ae3456679a8d1b4df26
Mateus Miguel <webmateusmiguel@gmail.com>
2018-04-02 14:47:07 -0300

    for

---
+++ controle/for.js
@@ -0,0 +1,12 @@
+for(let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+//usar let no i para não vazar
+for(let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`)
+}
+
+console.log(i)
