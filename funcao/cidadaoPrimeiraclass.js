6ff7f7a8addcc570f7b09e169a80928060ddb7bf
Mateus Miguel <webmateusmiguel@gmail.com>
2018-04-04 16:23:02 -0300

    Funções

--- funcao/cidadaoPrimeiraclass.js
+++ funcao/cidadaoPrimeiraclass.js
@@ -1,6 +1,3 @@
-//Função em JS é First-Class Object (Citizens)
-// High-order function
-
 //  Na função tradicional não é possível ocultar o bloco
 
 // função de foma literal
@@ -8,3 +5,30 @@ function fun1() { }
 
 //armazenar em variável - função anonima e atribui  a const
 const fun2 = function () {}
+
+// armazenar em array
+const array = [function (a, b) { return a + b}, fun1, fun2]
+console.log(array[0](2, 3))
+
+// Armazenamento em um atributo de objeto
+const obj = {}
+obj.falar = function() { return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como param 
+function run(fun) {
+    fun()
+}
+
+run(function() { console.log('Executando...')})
+
+//uma função pode conter outra função
+function soma (a, b) {
+    return function(c) {
+        console.log(a + b  + c)
+    }
+}
+
+soma (2, 3)(4)
+const cincoMais = soma(2, 3)
+cincoMais(4)
