8270f62931526792a90a30a61a4dd6668411463d
Mateus Miguel <webmateusmiguel@gmail.com>
2019-02-08 00:08:15 -0200

    Calculator

--- react/calculadora/src/main/Calculator.js
+++ react/calculadora/src/main/Calculator.js
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 import "./Calculator.css";
 
+import Button from "../components/Button";
+import Display from "../components/Display";
+
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.clearMemory = this.clearMemory.bind(this);
+    this.setOperation = this.setOperation.bind(this);
+    this.addDigit = this.addDigit.bind(this);
+  }
+
+  clearMemory() {
+    console.log("limpar");
+  }
+
+  setOpdration(operation) {
+    console.log(operation);
+  }
+
+  addDigit(n) {
+    console.log(n);
+  }
+
   render() {
+    const addDigit = n => this.addDigit(n);
+    const setOperation = op => this.setOpration(op);
     return (
-      <>
-        <h1>Calculadora</h1>
-        <div className="calculator" />
-      </>
+      <div className="calculator">
+        <Display value={100} />
+        <Button label="AC" click={this.clearMemory} />
+        <Button label="/" click={this.setOperation} />
+        <Button label="7" click={this.addDigit} />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
+      </div>
     );
   }
 }
