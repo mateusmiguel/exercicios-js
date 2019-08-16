600f52d6863d52750f85e874b580c0ec7e971cfa
Mateus Miguel <webmateusmiguel@gmail.com>
2019-08-15 23:06:06 -0300

    Gravando e recuperando dados do localstorage

--- todo-rocketseat/app.js
+++ todo-rocketseat/app.js
@@ -2,7 +2,7 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var todos = ["Fazer café", "Estudar Javascript"];
+var todos = JSON.parse(localStorage.getItem("list_todos")) || [];
 
 buttonElement.onclick = addTodo;
 
@@ -30,15 +30,20 @@ function renderTodos() {
 
 function addTodo() {
   var todoText = inputElement.value;
-
   todos.push(todoText);
   inputElement.value = "";
   renderTodos();
+  saveToStorage();
 }
 
 function deleteTodo(pos) {
   todos.splice(pos, 1);
   renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem("list_todos", JSON.stringify(todos));
 }
 
 renderTodos();
