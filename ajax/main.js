c80066b2061c8f37ad965f8367738052dd91a69a
Mateus Miguel <webmateusmiguel@gmail.com>
2019-09-01 14:25:58 -0300

    Using Axios

--- ajax/main.js
+++ ajax/main.js
@@ -1,22 +1,5 @@
-var minhaPromisse = function() {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://api.github.com/users/mateusmiguel");
-    xhr.send(null);
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject("Erro na requisição");
-        }
-      }
-    };
-  });
-};
-
-minhaPromisse()
+axios
+  .get("http://api.github.com/users/mateusmiguel")
   .then(function(response) {
     console.log(response);
   })
