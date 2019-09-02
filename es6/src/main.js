995bba08ed18ef6c236d07e4128417bf160bafc8
Mateus Miguel <webmateusmiguel@gmail.com>
2019-09-02 08:23:33 -0300

    Aulas

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,18 +1,33 @@
-import axios from "axios";
+// import axios from "axios";
 
-class Api {
-  static async getUserInfo(username) {
-    try {
-      const response = await axios.get(
-        `https://api.github.com/users/${username}`
-      );
+// class Api {
+//   static async getUserInfo(username) {
+//     try {
+//       const response = await axios.get(
+//         `https://api.github.com/users/${username}`
+//       );
 
-      console.log(response);
-    } catch (err) {
-      console.warn("Erro na API");
-    }
-  }
-}
+//       console.log(response);
+//     } catch (err) {
+//       console.warn("Erro na API");
+//     }
+//   }
+// }
 
-Api.getUserInfo("mateusmiguel");
-Api.getUserInfo("mateusmiguelxxpto");
+// Api.getUserInfo("mateusmiguel");
+// Api.getUserInfo("mateusmiguelxxpto");
+
+const delay = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+
+const umPorSegundo = async () => {
+  console.log(await delay(console.log("1s")));
+  console.log(await delay(console.log("2s")));
+  console.log(await delay(console.log("3s")));
+};
+
+umPorSegundo();
